d6a0b3f093f1c48f82de2c331f98240246087cb7
Cameron <cameron.dwyer@oneplacesolutions.com>
2020-12-30 17:41:12 +1100

    Initial commit

---
+++ Outlook/RoamingSettings/script.js
@@ -0,0 +1,28 @@
+$("#get").click(get);
+$("#set").click(set);
+$("#save").click(save);
+
+function get() {
+    var settingName = $("#settingName").val();
+    var settingValue = Office.context.roamingSettings.get(settingName);
+    $("#settingValue").val(settingValue);
+    console.log(`The value of setting "${settingName}" is "${settingValue}".`);
+}
+
+function set() {
+    var settingName = $("#settingName").val();
+    var settingValue = $("#settingValue").val();
+    Office.context.roamingSettings.set(settingName, settingValue);
+    console.log(`Setting "${settingName}" set to value "${settingValue}".`);
+}
+
+function save() {
+    // Save settings in the mailbox to make it available in future sessions.
+    Office.context.roamingSettings.saveAsync(function(result) {
+        if (result.status !== Office.AsyncResultStatus.Succeeded) {
+        console.error(`Action failed with message ${result.error.message}`);
+        } else {
+        console.log(`Settings saved with status: ${result.status}`);
+        }
+    });
+}
